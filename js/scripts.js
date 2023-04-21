4d254b22aea14ba97853a604f5dcaa02b64ebde2
Sargurpreet <130497501+Sargurpreet@users.noreply.github.com>
2023-04-21 14:22:44 +0200

    Add pokemonList array
    If appleid, this commit will add data of four pokemons in pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-alert('Hello world');
+let pokemonList = [];
 
-let favoriteFood = 'Paella';
-document.write(favoriteFood);
+pokemonList [0] = {
+  name: 'Charizard',
+  height: '1.7 m',
+  type: ['fire' , 'flying']
+}
+
+pokemonList [1] = {
+  name: 'Pikachu',
+  height: '0.4 m',
+  type: ['Electric']
+}
+
+pokemonList [2] = {
+  name: 'Pidgeot',
+  height: '1.5 m',
+  type: ['flying' , 'Normal']
+}
+
+pokemonList [3] = {
+  name: 'Bulbasaur',
+  height: '0.7 m',
+  type: ['grass' , 'poison']
+}
