52e509d74f666c6ce3a05d5b360d742bf548014f
Sargurpreet <130497501+Sargurpreet@users.noreply.github.com>
2023-04-21 17:50:48 +0200

    Change the data type of height
    If applied, this commit will change the data type of height from string to integer, so that later we can add conditions on it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,25 @@
 let pokemonList = [
   {
     name: 'Charizard',
-    height: '1.7 m',
+    height: 1.7, 
     type: ['fire' , 'flying']
   },
 
   {
     name: 'Pikachu',
-    height: '0.4 m',
+    height: 0.4,
     type: ['Electric']
   },
 
   {
     name: 'Pidgeot',
-    height: '1.5 m',
+    height: 1.5,
     type: ['flying' , 'Normal']
   },
 
   {
     name: 'Bulbasaur',
-    height: '0.7 m',
+    height: 0.7,
     type: ['grass' , 'poison']
   }
 ];
