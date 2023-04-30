2ea7e00400518345f91de598026d7b4375042b22
Sargurpreet <130497501+Sargurpreet@users.noreply.github.com>
2023-04-30 12:33:49 +0200

    Creating a for Each loop to print data of pokemons
    *Creating a loop function that can take a array and print some data of that array(name and height), also tells if it`s big.

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,25 @@ let pokemonList = [
   }
 ];
 
+/*Creating a loop function that can take a array 
+and print some data of that array(name and height), 
+also tells if it`s big.
+*/
+function loopFunction (list) {
+
+  const Sentence = list.name + '(Height: ' + list.height +')';
+
+  if (list.height >= 1.0) {
+    document.write('<p>' + Sentence + ' - Wow, that is big!' + '</p>');
+    } else {
+      document.write('<p>' + Sentence + '</p>');
+    }
+}
+
+pokemonList.forEach(loopFunction);
+
+
+/*
 for (let i=0; i < pokemonList.length; i++) {
   const pokemon = pokemonList[i];
   const pokemonSentence = pokemon.name + '(Height: ' + pokemon.height +')';
@@ -33,3 +52,4 @@ for (let i=0; i < pokemonList.length; i++) {
     document.write('<p>' + pokemonSentence + '</p>');
   }
 }
+*/
